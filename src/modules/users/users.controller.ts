66dd4d5da32d0f4eb3a278fbdd52463e8767347c
LuizHLG <luizlelek08@gmail.com>
2024-04-28 14:22:25 -0300

    fix(auth): login and register endpoints should be public

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -3,11 +3,13 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { Response } from 'express';
+import { IsPublic } from 'src/auth/decorators/is-public.decorator';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @IsPublic()
   @Post()
   async create(@Res() res: Response, @Body() createUserDto: CreateUserDto) {
     return res.status(201).json(await this.usersService.create(createUserDto));
