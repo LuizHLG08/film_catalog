861aaa27083c84a1dc6e8531db828151a74c582e
LuizHLG <luizlelek08@gmail.com>
2024-04-29 17:32:34 -0300

    fix: connection fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,6 +18,7 @@ import { FilmsModule } from './modules/films/films.module';
       database: process.env.DB_DATABASE,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
+      synchronize: false,
       logging: false,
       entities: [__dirname + '/**/*.entity{.js, .ts}'],
     }),
